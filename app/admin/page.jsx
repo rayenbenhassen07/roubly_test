3274e84f1a9e187576536bc20d2fd6e079121b1c
rayenbenhassen07 <rayenbenhassen07@gmail.com>
2024-04-07 07:15:30 +0100

    desactivate the rules

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -3,7 +3,7 @@ import React, { useState,useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { useAdminPassword } from '@/hooks/useAdminPassword';
 
-const Page = () => {
+const page = () => {
     const { password, passwordStored, storePassword } = useAdminPassword();
 
     useEffect(() => {
@@ -132,4 +132,4 @@ const Page = () => {
     </>
     );
   };
-export default Page
+export default page
